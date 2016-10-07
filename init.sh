9241d8419f4e67e775c13921bc39b96e84b1aa75
bushido <bushido@club.kyutech.ac.jp>
2016-10-07 14:04:10 +0900

    Automatic OS determination function add

--- init.sh
+++ init.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+if  uname -a | grep 'Linux' ; then
+    LSOPTION="alias ls='ls --color=auto'"
+fi
+if  uname -a | grep 'BSD' ; then
+    LSOPTION="alias ls='ls -G'"
+fi
+if  uname -a | grep 'Darwin' ; then
+    LSOPTION="alias ls='ls -G'"
+fi
+cp bashrc $HOME/.bashrc
+echo $LSOPTION >> $HOME/.bashrc
+
 if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
     echo "you need install vim-plug."
     echo "Aborted init."
     exit 1
 fi
 
-mv bashrc $HOME/.bashrc
-mv vimrc  $HOME/.vimrc
+cp vimrc  $HOME/.vimrc
